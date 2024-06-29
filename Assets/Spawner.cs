824d10e2a839c22bd23c80f74dc8aaf7045a8bbc
theowr <lasmorsasplayer@gmail.com>
2024-06-29 01:29:39 -0300

    Generador Semi Funcional
    Puede generar los objetos y aparecen en escena pero no se ven por algun motivo

---
+++ Assets/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] Objetos;
+
+    void Start()
+    {
+        SpawnRandomObject();
+    }
+
+    void SpawnRandomObject()
+    {
+        if (Objetos.Length > 0)
+        {
+            int randomIndex = Random.Range(0, Objetos.Length);
+            GameObject selectedObject = Objetos[randomIndex];
+
+            GameObject spawnedObject = Instantiate(selectedObject, transform.position, Quaternion.identity);
+
+            // Scale the spawned object to 4 times its original size
+            spawnedObject.transform.localScale *= 20f;
+        }
+        else
+        {
+            Debug.LogWarning("No objects in the array to spawn.");
+        }
+    }
+}
