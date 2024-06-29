339bd2b9aa03a9bb020f29ccc3f42253a196eab8
theowr <lasmorsasplayer@gmail.com>
2024-06-29 00:35:41 -0300

    Update Plata.cs

--- Assets/Sripts/Plata.cs
+++ Assets/Sripts/Plata.cs
@@ -7,6 +7,9 @@ public class Plata : MonoBehaviour
     public int presupuesto;
     public int precio1;
     public int precio2;
+    private Text presupuestoTxt;
+    private Text precio1Txt;
+    private Text precio2Txt;
 
     public Color colorCorrecto = Color.green;
     public Color colorIncorrecto = Color.red;
@@ -15,10 +18,20 @@ public class Plata : MonoBehaviour
 
     void Start()
     {
-        
+        //randomizar precios
+        presupuesto = Random.Range(300, 1001);
+        precio1 = Random.Range(1, 551);
+        precio2 = Random.Range(1, 501);
 
+        //obtener objetos para texto
+        presupuestoTxt = GameObject.Find("PresupuestoTxt").GetComponent<Text>();
+        precio1Txt = GameObject.Find("Precio1").GetComponent<Text>();
+        precio2Txt = GameObject.Find("Precio2").GetComponent<Text>();
 
-
+        //poner respectivos texto en cada caja
+        presupuestoTxt.text = "Tu presupuesto es " + presupuesto.ToString();
+        precio1Txt.text = precio1.ToString();
+        precio2Txt.text = precio2.ToString();
 
         for (int i = 0; i < botones.Length; i++)
         {
